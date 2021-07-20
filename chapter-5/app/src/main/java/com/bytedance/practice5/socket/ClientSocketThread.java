e04bb9be1dc0c5e1de0b55e32b120dc87209cb77
Carlalee <1299976147@qq.com>
2021-07-21 01:29:29 +0800

    commit chapter-5

---
+++ chapter-5/app/src/main/java/com/bytedance/practice5/socket/ClientSocketThread.java
@@ -0,0 +1,38 @@
+package com.bytedance.practice5.socket;
+
+import android.app.Activity;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.net.Socket;
+
+public class ClientSocketThread extends Thread {
+    public ClientSocketThread(SocketActivity.SocketCallback callback) {
+        this.callback = callback;
+    }
+
+    private SocketActivity.SocketCallback callback;
+
+    //head请求内容
+    private static String content = "HEAD / HTTP/1.1\r\nHost:www.zju.edu.cn\r\n\r\n";
+
+
+    @Override
+    public void run() {
+        // TODO 6 用socket实现简单的HEAD请求（发送content）
+        //  将返回结果用callback.onresponse(result)进行展示
+        try {
+            Socket socket = new Socket("localhost", 30000); //服务器IP及端口
+//            BufferedOutputStream os = new BufferedOutputStream(socket.getOutputStream());
+//            BufferedInputStream is = new BufferedInputStream(socket.getInputStream()); // 读进
+
+            int len= -1;
+            callback.onResponse(content);
+            socket.close(); //关闭socket
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
