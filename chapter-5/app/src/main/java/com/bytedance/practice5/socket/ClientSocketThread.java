bfbf2776b805b1544019c42324c547bf82e76cca
Carlalee <1299976147@qq.com>
2021-07-21 03:06:02 +0800

    modify chapter-5

--- chapter-5/app/src/main/java/com/bytedance/practice5/socket/ClientSocketThread.java
+++ chapter-5/app/src/main/java/com/bytedance/practice5/socket/ClientSocketThread.java
@@ -6,6 +6,7 @@ import android.widget.Toast;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ClientSocketThread extends Thread {
@@ -24,9 +25,8 @@ public class ClientSocketThread extends Thread {
         // TODO 6 用socket实现简单的HEAD请求（发送content）
         //  将返回结果用callback.onresponse(result)进行展示
         try {
+            ServerSocket serverSocket = new ServerSocket(30000);
             Socket socket = new Socket("localhost", 30000); //服务器IP及端口
-//            BufferedOutputStream os = new BufferedOutputStream(socket.getOutputStream());
-//            BufferedInputStream is = new BufferedInputStream(socket.getInputStream()); // 读进
 
             int len= -1;
             callback.onResponse(content);
